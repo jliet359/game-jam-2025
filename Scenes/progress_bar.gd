918f0fcf86585167ee42ab33af496c3b1807864a
purestdiamond <kyle@iwoods.us>
2025-07-21 07:47:08 -0400

    Player Live Timer and death
    broke player invisibility when possessing an enemy

---
+++ Scenes/progress_bar.gd
@@ -0,0 +1,45 @@
+extends ProgressBar
+
+
+@onready var live_timer: Timer = $LiveTimer
+ 
+@export var total_duration: float = 2
+
+var health: float = 100.0
+var decay_rate: float
+var tick_rate: float = .05
+
+
+func _ready():
+	print("Total duration set to: ", total_duration)
+	init_health(health)
+	
+	live_timer.wait_time = tick_rate  # e.g., 0.1 seconds
+	live_timer.one_shot = false
+	live_timer.timeout.connect(_on_live_timer_timeout)
+	
+	decay_rate = health / (total_duration / tick_rate)
+	live_timer.start()
+	
+func init_health(_health):
+	health = _health
+	max_value = health
+	value = health
+	
+func _on_live_timer_timeout():
+	value -= decay_rate
+	if value <= 0:
+		value = 0
+		live_timer.stop()
+		player_died()
+		
+func reset_health():
+	value = health
+	if not live_timer.is_stopped():
+		return
+	live_timer.wait_time = 5.0
+	live_timer.start()
+	
+func player_died(): 
+	get_parent().player_died()
+		
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,6 +8,10 @@ const JUMP_VELOCITY = -400.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var player: CharacterBody2D = $"."
 @onready var timer: Timer = $Timer
+@onready var live: ProgressBar = $Live
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var player_dead = false
 
 func _ready():
 	add_to_group("player")
@@ -15,41 +19,39 @@ func _ready():
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if player_dead == false:
+		if not is_on_floor():
+			velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	# Get the input direction and handle the movement/deceleration.
-	# Gets input direction: -1,0,1
-	var direction := Input.get_axis("move_left", "move_right")
-	# Flip the Sprite
-	if direction > 0:
-		animated_sprite_2d.flip_h = false
-	elif direction < 0:
-		animated_sprite_2d.flip_h = true
-	# Play Animations
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite_2d.play("idle")
-		else: 
-			animated_sprite_2d.play("walk")
-	else:
-		animated_sprite_2d.play("jump")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		# Get the input direction and handle the movement/deceleration.
+		# Gets input direction: -1,0,1
+		var direction := Input.get_axis("move_left", "move_right")
+		# Flip the Sprite
+		if direction > 0:
+			animated_sprite_2d.flip_h = false
+		elif direction < 0:
+			animated_sprite_2d.flip_h = true
+		# Play Animations
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite_2d.play("idle")
+			else: 
+				animated_sprite_2d.play("walk")
+		else:
+			animated_sprite_2d.play("jump")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
 	
 func _on_possess_area_body_entered(body):
-
-	
-	
 	if body.is_in_group("enemies") and body.has_method("become_player") and body.can_be_possessed:
-		
+		live.reset_health()
 		body.become_player()
 		player.visible = false
 		timer.wait_time = 3.0
@@ -59,3 +61,8 @@ func _on_possess_area_body_entered(body):
 func _on_timer_timeout() -> void:
 	player.visible = true
 	player.set_process(true)
+	
+func player_died():
+	player_dead = true
+	animated_sprite_2d.stop()
+	animation_player.play("die")
